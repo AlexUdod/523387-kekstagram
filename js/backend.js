5848c3db6a738973f531b961f51317585d2239bc
Alexey Udod <bagatur8@gmail.com>
2018-12-24 22:33:12 +0200

    smth

---
+++ js/backend.js
@@ -0,0 +1,59 @@
+'use strict';
+
+(function () {
+	var SUCCESS_REQUEST = 200;
+
+	window.backend = {
+		loadData: function (url, onLoad, onError) {
+			var xhr = new XMLHttpRequest();
+			xhr.responseType = 'json';
+
+			xhr.addEventListener('load', function () {
+				if (xhr.status === SUCCESS_REQUEST) {
+					onLoad(xhr.response);
+				} else {
+				onError(observeErrors(xhr));
+				}
+			});
+			xhr.open('GET', url);
+			xhr.send();
+		},
+
+		postData: function (url, onLoad, onError, formElement) {
+			var oData = new FormData(formElement);
+			var xhr = new XMLHttpRequest();
+			xhr.open('POST', url);
+			xhr.onload = function () {
+				if (xhr.status === SUCCESS_REQUEST) {
+					onLoad(xhr.response);
+				} else {
+					onError(observeErrors(xhr));
+				}
+			};
+			xhr.send(oData);
+		}
+	};
+
+	var observeErrors = function (xhr) {
+		var error;
+		switch (xhr.status) {
+			case 400:
+				error = 'Wrong request' + ' ' + xhr.status;
+				break;
+			case 401:
+				error = 'No authorisation' + ' ' + xhr.status;
+				break;
+			case 404:
+				error = 'Nothing has been found' + ' ' + xhr.status;
+				break;
+			case 500:
+				error = 'Remoute troubles' + ' ' + xhr.status;
+				break;
+			default:
+				error = 'Error number: ' + xhr.status + ' ' + xhr.statusText;
+		}
+		return error;
+	};
+
+
+})();
--- js/form.js
+++ js/form.js
@@ -1,69 +1,94 @@
 'use strict';
 
 (function () {
-	var inputHashtags = document.querySelector('.text__hashtags');
-	var inputTextDescription = document.querySelector('.text__description');
+	var ESC_KEYCODE = 27;
 
-	inputHashtags.addEventListener('invalid', function (evt) {
-	 if (inputHashtags.validity.tooShort) {
-		inputHashtags.setCustomValidity('Хэш-тег должен состоять минимум из 2-х символов');
-		}	else if (inputHashtags.validity.patternMismatch) {
-		inputHashtags.setCustomValidity('Хэш-тег начинается с символа # (решётка) и разделяються пробелами, xеш-тег не может состоять только из одной решётки');
-		}	else if (inputHashtags.validity.tooLong) {
-		inputHashtags.setCustomValidity('Имя не должно превышать 105-ти символов');
-		} else if (inputHashtags.validity.valueMissing) {
-		inputHashtags.setCustomValidity('Обязательное поле');
-		} else if (inputHashtags.validity.customError) {
-		inputHashtags.setCustomValidity(inputHashtags.validationMessage);
+	var onInputHashtags = document.querySelector('.text__hashtags');
+	var onInputTextDescription = document.querySelector('.text__description');
+
+	onInputHashtags.addEventListener('invalid', function () {
+		onInputHashtags.style.border = '2px solid red';
+		if (onInputHashtags.validity.tooShort) {
+		onInputHashtags.setCustomValidity('Хэш-тег должен состоять минимум из 2-х символов');
+		}	else if (onInputHashtags.validity.patternMismatch) {
+		onInputHashtags.setCustomValidity('Хэш-тег начинается с символа # (решётка) и разделяються пробелами, xеш-тег не может состоять только из одной решётки');
+		}	else if (onInputHashtags.validity.tooLong) {
+		onInputHashtags.setCustomValidity('Имя не должно превышать 105-ти символов');
+		} else if (onInputHashtags.validity.valueMissing) {
+		onInputHashtags.setCustomValidity('Обязательное поле');
+		} else if (onInputHashtags.validity.customError) {
+		onInputHashtags.setCustomValidity(onInputHashtags.validationMessage);
 		}	else {
-	  inputHashtags.setCustomValidity('');
+		onInputHashtags.style.border = '2px solid white';
+		onInputHashtags.setCustomValidity('');
 		}
 	});
 
-	inputHashtags.addEventListener('input', function (evt) {
-	  var target = evt.target;
-	  var itemsMassiv = createMassivFromInputHashtags();
-	  var condition = createMirrorMassiveHashtags();
-	  for (var i = 0; i < itemsMassiv.length; i++) {  	
-	    if (itemsMassiv.length > 5) {
-		    target.setCustomValidity('Нельзя указать больше пяти хэш-тегов');
-		  } else if (itemsMassiv[i].length < 2 || itemsMassiv[i].length > 20) {
-		  	target.setCustomValidity('Xеш-тег не может состоять только из одной решётки, максимальная длина одного хэш-тега 20 символов, включая решётку');
-		  } else if (condition === true) {
-	      target.setCustomValidity('Oдин и тот же хэш-тег не может быть использован дважды');
-	    }	else {
-		  	target.setCustomValidity('');
-		  }
-	  }
+	onInputHashtags.addEventListener('input', function (evt) {
+		var target = evt.target;
+		var itemsMassiv = createMassivFromInputHashtags();
+		var condition = createMirrorMassiveHashtags();
+		onInputHashtags.style.border = '2px solid red';
+		for (var i = 0; i < itemsMassiv.length; i++) {
+			if (itemsMassiv.length > 5) {
+				target.setCustomValidity('Нельзя указать больше пяти хэш-тегов');
+			} else if (itemsMassiv[i].length < 2 || itemsMassiv[i].length > 20) {
+				target.setCustomValidity('Xеш-тег не может состоять только из одной решётки, максимальная длина одного хэш-тега 20 символов, включая решётку');
+			} else if (condition === true) {
+				target.setCustomValidity('Oдин и тот же хэш-тег не может быть использован дважды');
+			}	else {
+				target.style.border = '2px solid white';
+				target.setCustomValidity('');
+			}
+		}
+	});
+
+	onInputTextDescription.addEventListener('invalid', function () {
+		onInputTextDescription.style.border = '2px solid red';
+		if (onInputTextDescription.validity.valueMissing) {
+			onInputTextDescription.setCustomValidity('Обязательное поле');
+		}
+		else {
+			onInputTextDescription.style.border = '2px solid white';
+			onInputTextDescription.setCustomValidity('');
+		}
 	});
 
 	var createMirrorMassiveHashtags = function () {
-		var itemsMassiv = createMassivFromInputHashtags();
+		var hashtagsList = createMassivFromInputHashtags();
 		var mirrorMassiv = [];
 		var condition = false;
-		for (var i = 0; i < itemsMassiv.length; i++) {
-			if (mirrorMassiv.indexOf(itemsMassiv[i]) === -1) {
-				mirrorMassiv.push(itemsMassiv[i]);
+		for (var i = 0; i < hashtagsList.length; i++) {
+			if (mirrorMassiv.indexOf(hashtagsList[i]) === -1) {
+				mirrorMassiv.push(hashtagsList[i]);
 			} else {
 				condition = true;
 			}
 		}
-		return condition;
+		return mirrorMassiv;
 	};
 
 	var createMassivFromInputHashtags = function () {
-		var massivFromInputHashtags = inputHashtags.value.split(' ');
+		var massivFromInputHashtags = onInputHashtags.value.split(' ');
 		return massivFromInputHashtags;
 	};
 
 	var createMassivOnChange = function () {
-		inputHashtags.addEventListener('change', createMassivFromInputHashtags);
+		onInputHashtags.addEventListener('change', createMassivFromInputHashtags);
 	};
 
 	createMassivOnChange();
 
 	window.form = {
-		inputHashtags: inputHashtags,
-		inputTextDescription: inputTextDescription
+		inputHashtags: onInputHashtags,
+		inputTextDescription: onInputTextDescription,
+		ESC_KEYCODE: ESC_KEYCODE,
+		sendForm: function () {
+			if (onInputHashtags.validationMessage !== true) {
+				window.pictureEffects.imgUploadOverlay.classList.add('hidden');
+				onInputHashtags.value = '';
+				onInputTextDescription.textContent = '';
+			} 
+		}
 	};	
 })();
